63ce71163315961612ba38d8cda33edc9fca6b32
Carlos Manuel Chee Vázquez <carlos.chee@cobachbc.edu.mx>
2020-09-29 10:15:51 -0700

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -6,7 +6,7 @@ $(function () {
       // datumTokenizer: Bloodhound.tokenizers.whitespace,
       queryTokenizer: Bloodhound.tokenizers.whitespace,
 
-      prefetch: baseurl + 'assets/js/search.json'
+      prefetch: baseurl + '/assets/js/search.json'
     });
 
     $('#search-box').typeahead({
